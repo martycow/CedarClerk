1fcfb79efd5d0750d86b98cc4373d54d3eaf7040
Viacheslav Chudaev <schudaev@gmail.com>
2026-07-05 23:41:43 -0700

    Created solution and projects

---
+++ CedarClerk.Core/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace CedarClerk.Core;
+
+public class Class1
+{
+
+}
---
+++ CedarClerk.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace CedarClerk.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
